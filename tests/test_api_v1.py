a645c29a1fa68012020cd8a1d7570d8c57590ead
elena berdugo rodriguez <bcspamaranzero.com>
2025-12-23 12:39:37 +0100

    show error

--- tests/test_api_v1.py
+++ tests/test_api_v1.py
@@ -1,5 +1,7 @@
 import pytest
 import httpx
+import logging
+from fastapi import APIRouter, HTTPException
 
 BASE_URL = "http://127.0.0.1:8000/v1/notifications/"
 
@@ -13,19 +15,24 @@ valid_notification = {
 
 @pytest.mark.asyncio
 async def test_create_notification_success():
-    async with httpx.AsyncClient() as client:
-        response = await client.post(BASE_URL, json=valid_notification)
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(BASE_URL, json=valid_notification)
 
-        #assert response.status_code == 201, "Create notif fail"
-        assert response.status_code == 201, response.text
-        
-        data = response.json()
-        # Comprobamos que los datos devueltos coincidan
-        assert data["userId"] == valid_notification["userId"]
-        assert data["type"] == valid_notification["type"]
-        assert data["title"] == valid_notification["title"]
-        assert data["message"] == valid_notification["message"]
-        assert "id" in data  # Mongo devuelve el id como string
+            #assert response.status_code == 201, "Create notif fail"
+            assert response.status_code == 201, response.text
+
+            data = response.json()
+            # Comprobamos que los datos devueltos coincidan
+            assert data["userId"] == valid_notification["userId"]
+            assert data["type"] == valid_notification["type"]
+            assert data["title"] == valid_notification["title"]
+            assert data["message"] == valid_notification["message"]
+            assert "id" in data  # Mongo devuelve el id como string
+
+    except Exception as e:
+        logging.exception("Failed to create notification")
+        raise HTTPException(status_code=500, detail=str(e))
         
 
 
